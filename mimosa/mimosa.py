8d8039f96e8df6d5c7a775b4accb48c3aa6e0e2f
Benjamin Schlueter <bschlueter@lanl.gov>
2024-06-20 11:15:10 -0600

    input valdation for py wrapper and src cleanup

--- mimosa/mimosa.py
+++ mimosa/mimosa.py
@@ -8,26 +8,50 @@
 
 import subprocess
 import sys 
+import os
 
 THREAD_COUNT = 1
 
+def print_usage():
+    print("Usage: ./mimosa.py -source <source list> -dest <dest path> <flags>")
+    sys.exit()    
+
 # input validation
 
-# if sys.argv[1] is not "-sources" or "-dest" not in sys.argv:
-    
+if (len(sys.argv) < 5) or (sys.argv[1] != "-source") or ("-dest" not in sys.argv):
+    print_usage()
+
+dest_flag_index = sys.argv.index("-dest")
+
+# verify source directories exist
+
+for dir in sys.argv[2:dest_flag_index]:
+    if os.path.exists(dir) and os.path.isdir(dir):
+        continue
+    else:
+        print(f"ERROR: source path \"{dir}\" does not exist")
+        sys.exit()
 
 print('---------------------------------------------')
 print(f'Starting MIMOSA with:\n')
 print(f'\tnumber of threads: {THREAD_COUNT}\n')
-print(f'\tsource location: {sys.argv[2:]}\n')
-print(f'\troot of destination tree: {sys.argv[2]}\n')
+print(f'\tsource location(s): {sys.argv[2:dest_flag_index]}\n')
+print(f'\troot of destination tree: {sys.argv[dest_flag_index+1]}\n')
 print(f'\toutput/log location: testing/pwalk-output')
 print('---------------------------------------------')
 
-# subprocess.run(['pwalk-mfs', '-mimosa', f'-dop={THREAD_COUNT}', f'-source={sys.argv[1]}', f'-dest={sys.argv[2]}', '-output=testing/pwalk-output'])
+for dir in sys.argv[2:dest_flag_index]:
+    dest_path = f"{sys.argv[dest_flag_index + 1]}/{os.path.basename(dir)}"
+    
+    if os.path.exists(dest_path):
+        print(f"Warning: {dest_path} already exists ... using {dest_path}_mimosa instead")
+        dest_path = f"{dest_path}_mimosa"
+
+    print(f"Mapping source {dir} to dest {dest_path}")
+
+    # subprocess.run(['pwalk-mfs', '-mimosa', f'-dop={THREAD_COUNT}', f'-source={dir}', f'-dest={dest_path}', '-output=testing/pwalk-output'])
 
 
 # Run summary with data from pwalk log file
 
 
-print_usage()
--- mimosa/src/tmp/dummy.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-
-#include "../../../src/config/config.h"
-
-int main()
-{
-config_init(NULL, NULL);
-int a = 1;
-}
